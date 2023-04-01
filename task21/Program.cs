ae578f49fdf49f84deec44f6b4f0584543db3cda
Alexey <super.balerun@yandex.ru>
2023-04-01 14:32:20 +0200

    add all task

---
+++ task21/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+
+Console.Write("Enter X1: ");
+int X1 = int.Parse(Console.ReadLine()!);
+Console.Write("Enter Y1: ");
+int Y1 = int.Parse(Console.ReadLine()!);
+Console.Write("Enter Z1: ");
+int Z1 = int.Parse(Console.ReadLine()!);
+Console.Write("Enter X2: ");
+int X2 = int.Parse(Console.ReadLine()!);
+Console.Write("Enter Y2: ");
+int Y2 = int.Parse(Console.ReadLine()!);
+Console.Write("Enter Z2: ");
+int Z2 = int.Parse(Console.ReadLine()!);
+
+double ab = Math.Sqrt(Math.Pow((X2 - X1), 2) + Math.Pow((Y2 - Y1), 2) + Math.Pow((Z2 - Z1), 2));
+ab = Math.Round(ab, 2);
+Console.WriteLine(ab);
---
+++ task23/Program.cs
@@ -0,0 +1,20 @@
+﻿// 
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Clear();
+Console.Write("Введите число N: ");
+int numN = int.Parse(Console.ReadLine()!);
+int count = 1;
+
+Console.WriteLine($"таблица кубов чисел от 1 до {numN}:");
+
+while (count <= numN)
+{
+    Console.Write($"{count * count * count}");
+    if (count != numN) Console.Write(", ");
+    //Console.Write($" {Math.Pow((count), 3)} ");
+    count++;
+}
